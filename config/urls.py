640d94e66b648f70d48d61b254fc1fc70be562b1
doingmov <yohei13111@mail.ru>
2026-08-26 23:57:39 +0300

    Add URL paths for home, FAQ, and storage rules

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from config import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.HomeView.as_view(), name="home"),
+    path("faq/", views.FaqView.as_view(), name="faq"),
+    path("storage-rules/", views.StorageRulesView.as_view(), name="storage_rules"),
 ]
