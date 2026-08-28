25741a3dff6c08477e95d355fa9a2d78b694daac
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-08-28 11:11:22 +0300

    fix: serve MEDIA_URL in dev (warehouse photos 404)

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from config import views
@@ -13,3 +15,5 @@ urlpatterns = [
     path("orders/", include("apps.rentals.urls")),
     path("accounts/", include("apps.users.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
