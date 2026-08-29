997c6c28a83035b4e4842f25edc9cc5250b5b4e4
doingmov <yohei13111@mail.ru>
2026-08-29 16:33:48 +0300

    Add my-rent path to rentals URLs

--- apps/rentals/urls.py
+++ apps/rentals/urls.py
@@ -4,4 +4,5 @@ from apps.rentals import views
 
 urlpatterns = [
     path("wizard/", views.OrderWizard.as_view(), name="order_wizard"),
+    path("my-rent/", views.MyRentView.as_view(), name="my_rent"),
 ]
